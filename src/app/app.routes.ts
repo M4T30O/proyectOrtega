eb520e5815adcca9f9c559a18b9ea5686d8a3d43
M4T30O <mateoortega431@gmail.com>
2026-04-23 10:46:16 -0300

    Probando

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './paginas/inicio/inicio';
+import { Ofertas } from './paginas/ofertas/ofertas';
+import { Producto } from './paginas/producto/producto';
+import { Contacto } from './paginas/contacto/contacto';
+import { InicioSesion } from './auth/inicio-sesion/inicio-sesion';
+import { Registro } from './auth/registro/registro';
+import { Carrito } from './compartidos/carrito/carrito';
+import { Favoritos } from './compartidos/favoritos/favoritos';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'Inicio', pathMatch: 'full' },
+    { path: "Inicio", component: Inicio },
+    { path: "Ofertas", component: Ofertas },
+    { path: "Producto", component: Producto },
+    { path: "Contacto", component: Contacto },
+    { path: "InicioSesion", component: InicioSesion },
+    { path: "Registro", component: Registro },
+    { path: "Carrito", component: Carrito },
+    { path: "Favoritos", component: Favoritos }
+];
--- src/app/compartidos/menu/menu.ts
+++ src/app/compartidos/menu/menu.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './menu.html',
+  standalone: true,
   styleUrl: './menu.css',
 })
 export class Menu {}
