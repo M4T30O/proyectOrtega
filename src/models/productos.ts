7b5293ad9fef3b5335f5279094310c8139f8c8ee
M4T30O <mateoortega431@gmail.com>
2026-05-07 09:38:08 -0300

    Subiendo mi proyecto :V

---
+++ src/models/productos.ts
@@ -0,0 +1,11 @@
+export interface Productos {
+    id : number ;
+    nombre: string;
+    escritor: string;
+    descripcion: string;
+    precio: number;
+    stock: number;
+    img: string;
+    genero: string;
+    disponibilidad: boolean;
+}
